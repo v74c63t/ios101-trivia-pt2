0127718cf5733e0b80cada9fe9ffff4e4ecbf7bd
Vanessa Tang <vanesst5@uci.edu>
2023-10-12 17:10:19 -0700

    Initial commit/began writing TriviaQuestionService

---
+++ Trivia/TriviaQuestionService.swift
@@ -0,0 +1,14 @@
+//
+//  TriviaQuestionService.swift
+//  Trivia
+//
+//  Created by Vanessa Tang on 10/12/23.
+//
+
+import Foundation
+class TriviaQuestionService{
+    static func fetchQuestions(amount: Int, category: Int?, difficulty: String?, type: String?){
+        let base = "https://opentdb.com/api.php?"
+        var params = "amount=\(amount)"
+    }
+}
